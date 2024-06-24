f35853588c658c59d2be89cdcb7a99e6dc94a5a6
Shahima Firoz Khan <99420269+Shahimafiroz@users.noreply.github.com>
2024-06-24 11:19:04 +0530

    Hii!

---
+++ hii/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Im spinning lol!
+        </p>
+        <a
+          className="App-link"
+          href="https://shahimafiroz.hashnode.dev/about"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          bruhhh Bruhh!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
