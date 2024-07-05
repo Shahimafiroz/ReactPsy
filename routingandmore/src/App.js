8dcf5e29bb2814004455906acf59a8a3f1eb3c60
Shahima Firoz Khan <99420269+Shahimafiroz@users.noreply.github.com>
2024-07-05 13:36:06 +0530

    Routing

--- routingandmore/src/App.js
+++ routingandmore/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
+import PageNotFound from './components/PageNotFound';
+import Filter from './components/Filter';
+import Contact from './components/NestedRoutingContact';
+import Github from './components/contact/Github';
+import Insta from './components/contact/Instagram';
+import Twit from './components/contact/Twitter';
+import Login from './components/Login';
+import Protected from './components/Protected';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <BrowserRouter>
+        <NavBar></NavBar>
+        <Routes>
+          <Route path='/' element={<Protected Comp={Home} />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/user/' element={<Users />} />
+          <Route path='/user/:nm' element={<Users />} />
+          <Route path='/filter' element={<Filter />} />
+          <Route path='/*' element={<PageNotFound />} />
+          <Route path='/contact/' element={<Contact />}>
+            <Route path='git' element={<Github />} />
+            <Route path='insta' element={<Insta />} />
+            <Route path='twt' element={<Twit />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
