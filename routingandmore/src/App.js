f77fab1b1ac97169a643e10793a6ce5ff95defd9
SHAHIMA FIROZ KHAN <99420269+Shahimafiroz@users.noreply.github.com>
2024-07-07 17:26:24 +0530

    routing from home

--- routingandmore/src/App.js
+++ routingandmore/src/App.js
@@ -1,38 +1,23 @@
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import NavBar from './components/NavBar';
-import Home from './components/Home';
-import About from './components/About';
-import Users from './components/Users';
-import PageNotFound from './components/PageNotFound';
-import Filter from './components/Filter';
-import Contact from './components/NestedRoutingContact';
-import Github from './components/contact/Github';
-import Insta from './components/contact/Instagram';
-import Twit from './components/contact/Twitter';
-import Login from './components/Login';
-import Protected from './components/Protected';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components /Home";
+import About from "./components /About";
+import Navbar from "./components /Navbar";
+import PageNotFOund from "./components /PageNotFOund";
+import Redirected from "./components /Redirected";
 
 function App() {
   return (
     <div className="App">
-
+      <h1 style={{ color: "red" }}> This is from the app component</h1>
       <BrowserRouter>
-        <NavBar></NavBar>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Protected Comp={Home} />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/user/' element={<Users />} />
-          <Route path='/user/:nm' element={<Users />} />
-          <Route path='/filter' element={<Filter />} />
-          <Route path='/*' element={<PageNotFound />} />
-          <Route path='/contact/' element={<Contact />}>
-            <Route path='git' element={<Github />} />
-            <Route path='insta' element={<Insta />} />
-            <Route path='twt' element={<Twit />} />
-          </Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/redi" element={<Redirected />} />
+          <Route path="/*" element={<PageNotFOund />} />
+          {/* <Route path="/*" element={<Navigate to="/redi" />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
---
+++ routingandmore/src/components /Redirected.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Redirected() {
+  return (
+    <div>
+      <h1>This is a redirected Page</h1>
+    </div>
+  );
+}
+
+export default Redirected;
